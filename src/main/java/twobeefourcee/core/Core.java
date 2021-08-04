45de3eb2b85a227a659dc94057ec891d707532cc
friend <friend@yourfriend.lv>
2021-08-04 21:59:18 +0300

    do like everything lol

--- src/main/java/twobeefourcee/core/Core.java
+++ src/main/java/twobeefourcee/core/Core.java
@@ -1,31 +1,93 @@
 
 package twobeefourcee.core;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.configuration.MemorySection;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import twobeefourcee.core.commands.Update;
 
-@SuppressWarnings({ "rawtypes" })
 public class Core extends JavaPlugin {
-	// DB db = DBMaker.fileDB("file.db").make();
-	// ConcurrentMap map = db.hashMap("map").createOrOpen();
+HashMap<Location, Material> locations = new HashMap<Location, Material>();
+
+	FileConfiguration config = this.getConfig();
 
 	@Override
 	public void onEnable() {
+		
+		this.saveDefaultConfig();
+		
+		Map<String, Object> worlds = this.getConfig().getConfigurationSection("worlds").getValues(false);
+
+		worlds.forEach((world, section) -> {
+			World wworld = Bukkit.getWorld(world);
+			
+			if(wworld == null) {
+				System.out.println("Configuration faulty. World " + world + " doesn't exist.");
+			} else {
+				MemorySection ssection = (MemorySection) section;
+				
+				Map<String, Object> blocks = ssection.getValues(true);
+				
+				blocks.forEach((material, locations) -> {
+					Material mmaterial = Material.matchMaterial(material);
+					@SuppressWarnings("unchecked")
+					ArrayList<String> locationss = (ArrayList<String>) locations;
+					
+					locationss.forEach(location -> {
+						String[] llocation = ((String)location).split(",");
+						
+						Location lllocation = new Location(wworld,Integer.parseInt(llocation[0]), Integer.parseInt(llocation[1]), Integer.parseInt(llocation[2]));
+						
+						if(mmaterial == null) {
+							System.out.println("Could not find " + material + " material!");
+						} else {
+							this.locations.put(lllocation, mmaterial);
+							System.out.println(lllocation);
+							System.out.println(mmaterial);
+						}
+					});
+				});
+			}
+		});
 
 		this.getCommand("update").setExecutor(new Update());
-		System.out.println(" i am online ");
-		if (Bukkit.getOnlinePlayers().size() != 0) {
-			System.out.println(
-					"I'm not really a fan of reloads, but because this is probably a development environment, I don't care.");
-			// do some shit here idk
-		}
-	}
 
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(this, new Runnable() {
+			public void run() {
+				locations.forEach((loc, mt) -> {
+					System.out.println(loc);
+					loc.getWorld().dropItemNaturally(loc, new ItemStack(mt));
+				});
+				
+			}
+		}, 0L, 3);
+	}
+	
+	public Material lookupMaterial(String s) {
+        try {
+            Package cbroot = getServer().getClass().getPackage();
+            Class<?> CraftMagicNumbers = Class.forName(cbroot.getName()+".util.CraftMagicNumbers", false, getServer().getClass().getClassLoader());
+            Method m = CraftMagicNumbers.getMethod("getMaterialFromInternalName", String.class);
+            Material material = (Material) m.invoke(null, s);
+            return material;
+        } catch (NoSuchMethodException | SecurityException | ClassNotFoundException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            return null;
+        }
+    }
 	@Override
 	public void onDisable() {
 		System.out.println(";( bye");
-		// db.close();
 	}
 }
--- src/main/java/twobeefourcee/core/commands/Update.java
+++ src/main/java/twobeefourcee/core/commands/Update.java
@@ -1,20 +1,65 @@
 package twobeefourcee.core.commands;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
-import twobeefourcee.core.Core;
+import twobeefourcee.core.UnzipUtility;
 
 public class Update implements CommandExecutor {
+
+	UnzipUtility unzipper = new UnzipUtility();
+
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		if (sender.isOp()) {
-			String path = Core.class.getProtectionDomain().getCodeSource().getLocation().getPath();
-			Bukkit.broadcastMessage("Deleting " + path);
+			try {
+				Bukkit.broadcastMessage("Started updating.");
+
+				if (download("https://nightly.link/yourfriendoss/2b4c/workflows/build/main/2b4cCore.zip",
+						"2b4cCoreUPDATE.zip") != 0) {
+					Bukkit.broadcastMessage("Zip downloaded.");
+
+					try {
+						unzipper.unzip("2b4cCoreUPDATE.zip", "plugins/");
+						File zip = new File("2b4cCoreUPDATE.zip");
+
+						zip.delete();
+
+						Bukkit.broadcastMessage("Zip unpacked.");
+
+						System.gc();
+						Bukkit.reload();
+
+						Bukkit.broadcastMessage("I've attempted reloading the server. Update succeeded.");
+
+					} catch (Exception ex) {
+						Bukkit.broadcastMessage("Zip failed unpacking ");
+						ex.printStackTrace();
+					}
+
+				} else {
+					Bukkit.broadcastMessage("Download failed.");
+				}
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 
 		}
-		// If the player (or console) uses our command correct, we can return true
 		return true;
+	}
+
+	static long download(String sourceUrl, String targetFileName) throws Exception {
+		try (InputStream in = URI.create(sourceUrl).toURL().openStream()) {
+			return Files.copy(in, Paths.get(targetFileName), StandardCopyOption.REPLACE_EXISTING);
+		}
 	}
 }
