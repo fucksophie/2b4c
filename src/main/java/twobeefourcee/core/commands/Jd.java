6427a5b58f341b498b94746a5223304353acb910
friend <friend@yourfriend.lv>
2021-08-29 21:11:06 +0300

    add joindate

---
+++ src/main/java/twobeefourcee/core/commands/Jd.java
@@ -0,0 +1,38 @@
+package twobeefourcee.core.commands;
+
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import twobeefourcee.core.Core;
+
+
+public class Jd implements CommandExecutor {
+
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(args.length == 1) {
+			Player player = Bukkit.getPlayer(args[0]);
+			if(player != null) {
+				long time = Core.database.getLong(player.getUniqueId().toString() + ".joinDate");
+				DateTimeFormatter formatter = DateTimeFormatter
+						.ofLocalizedDateTime(FormatStyle.SHORT)
+						.withZone(ZoneId.from(ZoneOffset.UTC));
+				
+				sender.sendMessage(Core.success + "Player " + player.getName() + " joined " + formatter.format(Instant.ofEpochSecond(time)));
+			} else {
+				sender.sendMessage(Core.error + "Player is not online.");
+			}
+		}
+
+		return true;
+	}
+}
--- src/main/java/twobeefourcee/core/events/JoinMessages.java
+++ src/main/java/twobeefourcee/core/events/JoinMessages.java
@@ -1,5 +1,8 @@
 package twobeefourcee.core.events;
 
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -13,8 +16,23 @@ public class JoinMessages implements Listener {
     @EventHandler
     public void joinEvent(PlayerJoinEvent event) {
     	Player player = event.getPlayer();
-    
-    	event.setJoinMessage(Core.success + player.getName() + " joined.");
+
+    	if(Core.database.getLong(player.getUniqueId().toString() + ".joinDate") == 0) {
+    		Bukkit.broadcastMessage(Core.info + player.getName() + " joined for the first time.");
+    		long unixTime = System.currentTimeMillis() / 1000L; // Using this as a long protects against Y38.
+    		
+    		Core.database.set(player.getUniqueId().toString() + ".joinDate", unixTime);
+		
+			try {
+				Core.database.save(Core.databaseFile);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+		    
+	    	event.setJoinMessage(Core.success + player.getName() + " joined.");
+	    	
+		}
     }
     
     @EventHandler
