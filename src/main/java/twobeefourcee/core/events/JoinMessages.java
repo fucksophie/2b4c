d626dc973abcec86a05a6c4d07352e17c5aaa724
friend <friend@yourfriend.lv>
2021-08-29 15:35:42 +0300

    add swaggy join messages (swag)

--- src/main/java/twobeefourcee/core/events/JoinMessages.java
+++ src/main/java/twobeefourcee/core/events/JoinMessages.java
@@ -1,5 +1,26 @@
 package twobeefourcee.core.events;
 
-public class JoinMessages {
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
+import twobeefourcee.core.Core;
+
+public class JoinMessages implements Listener {
+	
+    @EventHandler
+    public void joinEvent(PlayerJoinEvent event) {
+    	Player player = event.getPlayer();
+    	
+    	event.setJoinMessage(Core.success + player.getName() + " joined.");
+    }
+    
+    @EventHandler
+    public void leaveEvent(PlayerQuitEvent event) {
+    	Player player = event.getPlayer();
+    	
+    	event.setQuitMessage(Core.error + player.getName() + " left.");
+    }
 }
