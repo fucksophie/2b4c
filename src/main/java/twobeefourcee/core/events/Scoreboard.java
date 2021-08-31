b770d0817a9310328b0866d07705a2789a97489b
friend <friend@yourfriend.lv>
2021-08-31 12:31:02 +0300

    Optimize the Scoreboard

--- src/main/java/twobeefourcee/core/events/Scoreboard.java
+++ src/main/java/twobeefourcee/core/events/Scoreboard.java
@@ -6,6 +6,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 import fr.minuskube.netherboard.Netherboard;
 import fr.minuskube.netherboard.bukkit.BPlayerBoard;
@@ -19,16 +20,36 @@ public class Scoreboard implements Listener {
     	Player player = event.getPlayer();
     	BPlayerBoard board = Netherboard.instance().createBoard(player, Core.error);
     	 
-    	Bukkit.getScheduler().scheduleSyncRepeatingTask(Core.getPlugin(Core.class), new Runnable() {
-    	    @Override
-    	    public void run() {
-    	    	int seconds = player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+    	int seconds = player.getStatistic(Statistic.PLAY_ONE_MINUTE) / 20;
+    	
+    	board.set(ChatColor.translateAlternateColorCodes('&', "&cYou have played for &l" + seconds/86400 + "d, " + seconds%86400/3600 + "h."), 2);
+    	board.set(ChatColor.translateAlternateColorCodes('&', "&cThere are &l" + Bukkit.getOnlinePlayers().size() + " players online."), 3);
+    	
+    	board.set("", 1);
+    	
+    	board.set(ChatColor.translateAlternateColorCodes('&', "&cdiscord.gg/&4zXyQ7WsyJp"), 0);
+    	
+    	Bukkit.getOnlinePlayers().forEach(playerr -> {
+    		if(player != playerr) {
+    			BPlayerBoard bboard = Netherboard.instance().getBoard(playerr);
     	    	
-    	    	board.set(ChatColor.translateAlternateColorCodes('&', "&cYou have played for &l" + seconds/86400 + "d, " + seconds%86400/3600 + "h."), 2);
-    	    	board.set(ChatColor.translateAlternateColorCodes('&', "&cThere are &l" + Bukkit.getOnlinePlayers().size() + " players online."), 3);
-    	    	board.set("", 1);
-    	    	board.set(ChatColor.translateAlternateColorCodes('&', "&cdiscord.gg/&4zXyQ7WsyJp"), 0);
-    	    }
-    	}, 0L, 20L);
+    			bboard.set(ChatColor.translateAlternateColorCodes('&', "&cThere are &l" + Bukkit.getOnlinePlayers().size() + " players online."), 3);
+
+    		};
+    	});
+    }
+    
+    @EventHandler
+    public void leaveEvent(PlayerQuitEvent event) {
+    	Player player = event.getPlayer();
+    	
+    	Bukkit.getOnlinePlayers().forEach(playerr -> {
+    		if(player != playerr) {
+    			BPlayerBoard board = Netherboard.instance().getBoard(playerr);
+    	    	
+    			board.set(ChatColor.translateAlternateColorCodes('&', "&cThere are &l" + Bukkit.getOnlinePlayers().size() + " players online."), 3);
+
+    		};
+    	});
     }
 }
